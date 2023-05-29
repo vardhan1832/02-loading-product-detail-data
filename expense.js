f9a93a1aec4925fd7ad34824c1116cab2871e79f
vardhan1831 <sreevardhan259@gmail.com>
2023-05-29 13:16:00 +0530

    deploy-basics

--- expense.js
+++ expense.js
@@ -1,7 +1,11 @@
-// const path = require('path');
+const path = require('path');
+const fs = require('fs')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet  = require('helmet')
+const compression = require('compression')
+const morgan = require('morgan')
 
 const sequelize = require('./util/database')
 
@@ -22,6 +26,11 @@ const expenseroutes = require('./routes/expenseTracker')
 const purchaseroutes = require('./routes/purchase')
 const premiumfeatureroutes = require('./routes/premiumfeature')
 
+const filestreamaccess = fs.createWriteStream(path.join(__dirname,'access.log'),{flags: 'a'})
+
+app.use(helmet())
+app.use(compression())
+app.use(morgan('combined',{stream: filestreamaccess}))
 
 app.use(bodyParser.json({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +41,8 @@ app.use(loginroutes)
 app.use('/user',expenseroutes)
 app.use(purchaseroutes)
 app.use(premiumfeatureroutes)
+
+//console.log(process.env.NODE_ENV)
 
 User.hasMany(Expense)
 Expense.belongsTo(User)
@@ -49,7 +60,7 @@ DownloadedFiles.belongsTo(User)
 sequelize.sync()
 .then(res=>{
     // console.log(res)
-    app.listen(4000)
+    app.listen(process.env.PORT || 4000)
 })
 .catch(err=>{
     console.log(err);
