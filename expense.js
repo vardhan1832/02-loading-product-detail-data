a46941aa6bdbf1524c0ef4575f799e72bf69fbdd
vardhan1831 <sreevardhan259@gmail.com>
2023-06-01 13:55:49 +0530

    as

--- expense.js
+++ expense.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet  = require('helmet')
-const compression = require('compression')
 const morgan = require('morgan')
 require('dotenv').config();
 
@@ -30,7 +29,6 @@ const premiumfeatureroutes = require('./routes/premiumfeature')
 const filestreamaccess = fs.createWriteStream(path.join(__dirname,'access.log'),{flags: 'a'})
 
 app.use(helmet())
-app.use(compression())
 app.use(morgan('combined',{stream: filestreamaccess}))
 
 app.use(bodyParser.json({ extended: false }));
